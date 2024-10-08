b7bc19859eb07e26567dc6e93eca8c15ded9f1cd
GLFABILA <glfabila2@gmail.com>
2024-10-08 11:10:25 +0800

    updated all shts

--- name-logging-applet/script.js
+++ name-logging-applet/script.js
@@ -1,5 +1,3 @@
-alert('Script is loaded');
-
 const nameInput = document.getElementById('nameInput');
 const saveButton = document.getElementById('saveButton');
 const gallery = document.getElementById('gallery');
@@ -7,20 +5,21 @@ const totalLogs = document.getElementById('totalLogs');
 
 saveButton.addEventListener('click', () => {
     const name = nameInput.value.trim();
+    const date = new Date().toLocaleString();
+
     if (name) {
         const cardCol = document.createElement('div');
         cardCol.className = 'col-md-4 mb-3';
 
         const card = document.createElement('div');
         card.className = 'card text-center';
-        card.style.cursor = 'pointer';
 
         const cardBody = document.createElement('div');
         cardBody.className = 'card-body';
 
         const cardTitle = document.createElement('h5');
         cardTitle.className = 'card-title';
-        cardTitle.textContent = `Hello, ${name}`;
+        cardTitle.textContent = `Hello, ${name} - ${date}`;
 
         card.addEventListener('click', () => {
             alert(`You clicked on: ${name}`);
